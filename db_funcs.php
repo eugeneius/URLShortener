94820703d733de87922fe24521fb58479ace56d6
Eoin Murphy <eoin@murph.ie>
2011-09-04 20:19:11 +0100

    added redirects counter field to the table schema, and associated code. Also added function for printing table.

--- db_funcs.php
+++ db_funcs.php
@@ -4,7 +4,6 @@
  * Connect to the db
  */
 function connect($url, $name, $user, $pass) {
-  //Connect to the DB
   mysql_connect($url, $user, $pass) or die(mysql_error());
   mysql_select_db($name) or die(mysql_error());
 }
@@ -17,11 +16,10 @@ function disconnect() {
 }
 
 /*
- * Create the tables
+ * Create the table
  */
-function createTables() {
-  //Create the table
-  mysql_query("CREATE TABLE dictionary(id INT NOT NULL AUTO_INCREMENT, url VARCHAR(255), PRIMARY KEY(id), KEY(url, id))");
+function createTable() {
+  mysql_query("CREATE TABLE dictionary(id INT NOT NULL AUTO_INCREMENT, url VARCHAR(255), redirects INT, PRIMARY KEY(id), KEY(url, id))");
 }
 
 /*
@@ -36,7 +34,7 @@ function insertURL($url) {
   //If it doesn't, add it
   if (!isset($id)) {
     //Add to the dictionary
-    mysql_query("INSERT INTO dictionary(url) VALUES('".$url."') ") or die(mysql_error());
+    mysql_query("INSERT INTO dictionary(url,redirects) VALUES('".$url."',0) ") or die(mysql_error());
 
     //Get the new id
     $return = mysql_query("SELECT id FROM dictionary ORDER BY id DESC LIMIT 1");
@@ -52,16 +50,48 @@ function insertURL($url) {
  * Retrieve the url using the id
  */
 function retrieveURL($id) {
+  mysql_query("UPDATE dictionary SET redirects=redirects+1 WHERE id='".$id."'") or die(mysql_error());
   $return = mysql_query("SELECT url FROM dictionary WHERE id='".$id."'") or die(mysql_error());
   $return = mysql_fetch_array($return);
   return $return['url'];
 }
 
 /*
+ * Print the table 
+ */
+function printTable() {
+  $return = mysql_query("SELECT * FROM dictionary") or die(mysql_error());
+  $fieldCount = mysql_num_fields($return);
+
+  echo "<h1>dictionary</h1>\n";
+  echo "<table border='1'>\n  <tr>\n";
+  for ($i=0; $i < $fieldCount; $i++) {
+    $field = mysql_fetch_field($return);
+    echo "    <td>{$field->name}</td>\n";
+  }
+  echo "  </tr>\n";
+  
+  while ($row = mysql_fetch_row($return)) {
+    echo "  <tr>\n";
+    foreach($row as $cell)
+      echo "    <td>$cell</td>\n";
+    echo "  </tr>\n";
+  }
+  echo "</table>";
+}
+
+/*
  * Press in case of emergency
  */
-function clearTables() {
+function clearTable() {
   mysql_query("TRUNCATE TABLE dictionary") or die(mysql_error());
+}
+
+/*
+ * Even bigger red button
+ */
+function dropTable() {
+  mysql_query("DROP TABLE dictionary") or die(mysql_error());
 }
 
 ?>
